0ada87d585c310a4b98354c6cd2b2ceb27251582
gregordirks <92386941+Gregor499@users.noreply.github.com>
2024-12-02 19:02:07 +0200

    - added ec2 instance variable files

---
+++ terraform.tfvars
@@ -0,0 +1,3 @@
+ami_image_name = "ami-0ae9642bc1885d7b5"
+
+instance_type = "t2.micro"
---
+++ variables.tf
@@ -0,0 +1,17 @@
+variable "ami_image_name" {
+
+  default = "ami-0ae9642bc1885d7b5"
+
+  type = string
+
+  description = "This is a variable for AMI image"
+
+}
+
+variable "instance_type" {
+
+  type = string
+
+  description = "This is a variable for instance type"
+
+}
